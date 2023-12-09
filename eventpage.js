8291f6d7456c608cc2601b1f2ca74403502d9f78
htrinter <htdev@posteo.de>
2023-12-09 15:38:14 +0100

    upgrade to manifest version 3

--- eventpage.js
+++ eventpage.js
@@ -1,41 +1,58 @@
-chrome.runtime.onStartup.addListener(function(){
+chrome.runtime.onStartup.addListener(function() {
 	update();
 });
-chrome.runtime.onInstalled.addListener(function(){
+
+chrome.runtime.onInstalled.addListener(function() {
 	update();
 });
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
-	if(request.greeting == 'update') {
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+	if (request.greeting === 'update') {
 		update();
+		sendResponse({ reply: "updated" });
+		return true;
 	}
 });
 
-update();
-
 function update() {
-	chrome.browserAction.setBadgeBackgroundColor({color: [54, 163, 237, 255]});
+	chrome.action.setBadgeBackgroundColor({ color: [54, 163, 237, 255] });
 
 	chrome.storage.local.get('weather_temperature', function(result) {
-        chrome.browserAction.setBadgeText({text: result.weather_temperature});
-    });
-
-	chrome.alarms.clearAll(function(){});
-	
-	chrome.alarms.onAlarm.addListener(function(alarm){
-		$.get('https://www.uni-muenster.de/Klima/wetter/wetter.php', function(data) {
-	  		var weather = $('table.weather', data).html();
-	  		var weather_temperature = $('table.weather', data).find(".data.tab4").html();
-	  		if(weather_temperature != undefined && weather != undefined) {
-	  			chrome.storage.local.set({'weather': weather}, function(){});
-	  			chrome.storage.local.set({'weather_temperature': weather_temperature.split(" ")[0].trim()}, function() {
-					chrome.storage.local.get('weather_temperature', function(result){
-        				chrome.browserAction.setBadgeText({text: result.weather_temperature});
-    				});
-	  			});
-			}
-		});
+		chrome.action.setBadgeText({ text: result.weather_temperature || '' });
 	});
 
-	chrome.alarms.create({when: Date.now(), periodInMinutes: 10.0});
+	chrome.alarms.clearAll();
 
+	chrome.alarms.onAlarm.addListener(function(alarm) {
+		fetchWeatherData();
+	});
+
+	chrome.alarms.create({ when: Date.now(), periodInMinutes: 10.0 });
 }
+
+function fetchWeatherData() {
+	fetch('https://www.uni-muenster.de/Klima/wetter/wetter.php')
+		.then(response => response.text())
+		.then(data => {
+			const weatherStart = data.indexOf('<table class="weather">');
+			const weatherEnd = data.indexOf('</table>', weatherStart) + 8;
+			const weatherData = data.slice(weatherStart, weatherEnd);
+
+			const temperatureSelector = '<td class="data tab4" style="text-align:right;">';
+			const temperatureStart = data.indexOf(temperatureSelector) + temperatureSelector.length;
+			const temperatureEnd = data.indexOf('</td>', temperatureStart);
+			const weatherTemperature = data.slice(temperatureStart, temperatureEnd);
+
+			if (weatherTemperature && weatherData) {
+				chrome.storage.local.set({ 'weather': weatherData });
+				chrome.storage.local.set({ 'weather_temperature': weatherTemperature }, function() {
+					chrome.storage.local.get('weather_temperature', function(result) {
+						chrome.action.setBadgeText({ text: result.weather_temperature || '' });
+					});
+				});
+			}
+		})
+		.catch(error => console.error('Error fetching weather data:', error));
+}
+
+update();
